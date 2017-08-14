8b796df947a8408bc7f8fdcd04e7185615766181
James-Raymond <jamesmraymond7@gmail.com>
2017-08-14 21:38:07 +0100

    onclick listeners added

--- app/src/main/java/com/example/james/specieshunt/MainActivity.java
+++ app/src/main/java/com/example/james/specieshunt/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.james.specieshunt;
-
+import android.view.View.OnClickListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonFW = (Button) findViewById(R.id.buttonFW);
+        Button buttonSW = (Button) findViewById(R.id.buttonSW);
+        TextView scoreFW = (TextView) findViewById(R.id.scoreFW);
+        TextView scoreSW = (TextView) findViewById(R.id.scoreSW);
+
+        buttonFW.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonSW.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
